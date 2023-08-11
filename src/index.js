9def381b3750f896039321641a6a3c02e650b6ca
urvir12 <134030353+urvir12@users.noreply.github.com>
2023-08-11 14:49:02 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
